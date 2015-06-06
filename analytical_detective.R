9a2eeaec7253ac3afdf88285ef230c128b835f4c
jmarrietar <jmarrietar@gmail.com>
2015-06-05 21:08:57 -0500

    data

---
+++ analytical_detective.R
@@ -0,0 +1,60 @@
+################
+#QUICK QUESTION#  
+################
+setwd("/Users/josemiguelarrieta/Documents/Analytics_Edge")
+
+sd(c(5,8,12))
+which.min(c(4,1,6))
+Sys.setlocale("LC_ALL", "C")
+
+WHO<-read.csv("WHO.csv")
+
+which.min(WHO$Over60)
+WHO[183,]
+
+which.max(WHO$LiteracyRate)
+WHO[44,]
+
+#tapply 
+#So tapply splits the data by the second argument you give,
+#and then applies the third argument function
+#to the variable given as the first argument.
+
+tapply(WHO$Over60,WHO$Region,mean)
+
+#Which region has the lowest average child mortality rate across all countries in that region?
+tapply(WHO$ChildMortality,WHO$Region,mean)
+
+#######################
+#Analytical Detective## 
+#######################
+
+#This problem is focus on "GTA CRIME"-> This is the act of stealing, or attempting to steal, a car. 
+
+# DataFrame Columns Description. 
+
+# D: a unique identifier for each observation
+# Date: the date the crime occurred
+# LocationDescription: the location where the crime occurred
+# Arrest: whether or not an arrest was made for the crime (TRUE if an arrest was made, and FALSE if an arrest was not made)
+# Domestic: whether or not the crime was a domestic crime, meaning that it was committed against a family member (TRUE if it was domestic, and FALSE if it was not domestic)
+# Beat: the area, or "beat" in which the crime occurred. This is the smallest regional division defined by the Chicago police department.
+# District: the police district in which the crime occured. Each district is composed of many beats, and are defined by the Chicago Police Department.
+# CommunityArea: the community area in which the crime occurred. Since the 1920s, Chicago has been divided into what are called "community areas", of which there are now 77. The community areas were devised in an attempt to create socially homogeneous regions.
+# Year: the year in which the crime occurred.
+# Latitude: the latitude of the location at which the crime occurred.
+# Longitude: the longitude of the location at which the crime occurred.
+
+
+
+############
+#Load Data##
+############
+
+
+
+
+
+
+
+
