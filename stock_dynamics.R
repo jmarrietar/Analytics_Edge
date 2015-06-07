fce4f118027d19940b2ce65639f7d82cd9d234de
jmarrietar <jmarrietar@gmail.com>
2015-06-06 20:07:06 -0500

    data

--- stock_dynamics.R
+++ stock_dynamics.R
@@ -1,5 +1,10 @@
-setwd("/Users/josemiguelarrieta/Documents/Analytics_Edge")
+setwd("/Users/josemiguelarrieta/Documents/Analytics_Edge/")
 
 #A stock market is where buyers and sellers trade shares of a company, 
 #and is one of the most popular ways for individuals and companies to invest money.
 
+IBM<-read.csv("Data/IBMStock.csv")
+GE<-read.csv("Data/GEStock.csv")
+ProterGamble<-read.csv("Data/ProcterGambleStock.csv")
+CocaCola<-read.csv("Data/CocaColaStock.csv")
+Boeing<-read.csv("Data/BoeingStock.csv")
